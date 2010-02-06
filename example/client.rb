48c78214c3748dcab7fde350cdbe6bf35b26b08b
Tom Wilson <tom@jackhq.com>
2010-02-05 23:16:51 -0500

    added builder to require'

--- example/client.rb
+++ example/client.rb
@@ -1,12 +1,18 @@
-require 'openuri'
+require 'rest_client'
+require 'rexml/document'
+require 'open-uri'
 require 'base64'
 
 def convert(url_params)
   
   html_contents = RestClient.get url_params
-  resource = RestClient::Resource.new('http://76a05e277f@htmldoc.jackfile.com', :timeout => 1000000)
+  puts html_contents
+  #resource = RestClient::Resource.new('http://76a05e277f@htmldoc.jackfile.com', :timeout => 1000000)
+  resource = RestClient::Resource.new('http://76a05e277f@htmldoc.jackfile.com:9292', :timeout => 1000000)
+
   response = resource.post(:html_contents =>  html_contents)
   doc = REXML::Document.new(response)  
+  puts doc
   return Base64.decode64(doc.elements['/pdf/contents'].text)
   
 end
--- html2pdf.rb
+++ html2pdf.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'base64'
+require 'builder'
 
 ####################################################
 #
